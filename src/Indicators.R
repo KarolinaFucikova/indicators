8b0266a6c5622800442feb7bc397f8124251fdcb
Karolina Fucikova <karolina.fucikova@gmail.com>
2023-09-17 19:52:04 -0400

    first attempt at indicator analysis for Harriman data

--- src/Indicators.R
+++ src/Indicators.R
@@ -4,6 +4,7 @@ library(indicspecies)
 #example dataset:
 data(wetland)
 
+# species are in columns
 #otherwise:
 #wetland <- read.csv("data/INSERTNAME.csv", row.names = 1)
 
@@ -22,3 +23,25 @@ groupskm
 indval = multipatt(wetland, groups, 
                    control = how(nperm=999)) 
 summary(indval) 
+
+#################
+# Harriman data
+harriman <- read.csv("data/Harriman_species.csv", row.names=1)
+
+# replace NAs with 0s
+harriman[is.na(harriman)] <- 0
+# transpose next so that species are in columns
+harriman <- t(harriman)
+
+# could try grouping into natural vs. artificial lakes
+
+# or nonhierarchical clustering
+harkm = kmeans(harriman, centers=3)
+groupskm = harkm$cluster
+groupskm
+# indicator analysis - will determine indicator values for each species
+# i.e., how strongly the species is associated with a particular group/category
+indval = multipatt(harriman, groupskm, 
+                   control = how(nperm=999)) 
+summary(indval) 
+
